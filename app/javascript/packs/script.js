1e6c968800bb39f5cc528a9de35ffd7294aa987b
Jess Pates <jesspates@hotmail.co.uk>
2023-03-25 21:32:47 +0100

    js added for modals

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -22,3 +22,133 @@ document.addEventListener("keydown", function (e) {
     closeHangmanWindow();
   }
 });
+
+
+const flashWindow = document.querySelector(".flash-window");
+const showFlash = document.querySelector(".show-flash");
+const closeFlash = document.querySelector(".close-flash");
+const overlayFlash = document.querySelector(".overlay-flash");
+
+const closeFlashWindow = function() {
+  flashWindow.classList.add("hidden");
+  overlayFlash.classList.add("hidden");
+};
+
+showFlash.addEventListener("click", function () {
+  flashWindow.classList.remove("hidden");
+  overlayFlash.classList.remove("hidden");
+});
+
+closeFlash.addEventListener("click", closeFlashWindow);
+
+overlayFlash.addEventListener("click", closeFlashWindow);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !flashWindow.classList.contains("hidden")) {
+    closeFlashWindow();
+  }
+});
+
+
+const oxWindow = document.querySelector(".ox-window");
+const showOx = document.querySelector(".show-ox");
+const closeOx = document.querySelector(".close-ox");
+const overlayOx = document.querySelector(".overlay-ox");
+
+const closeOxWindow = function() {
+  oxWindow.classList.add("hidden");
+  overlayOx.classList.add("hidden");
+};
+
+showOx.addEventListener("click", function () {
+  oxWindow.classList.remove("hidden");
+  overlayOx.classList.remove("hidden");
+});
+
+closeOx.addEventListener("click", closeOxWindow);
+
+overlayOx.addEventListener("click", closeOxWindow);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !oxWindow.classList.contains("hidden")) {
+    closeOxWindow();
+  }
+});
+
+
+const mineWindow = document.querySelector(".mine-window");
+const showMine = document.querySelector(".show-mine");
+const closeMine = document.querySelector(".close-mine");
+const overlayMine = document.querySelector(".overlay-mine");
+
+const closeMineWindow = function() {
+  mineWindow.classList.add("hidden");
+  overlayMine.classList.add("hidden");
+};
+
+showMine.addEventListener("click", function () {
+  mineWindow.classList.remove("hidden");
+  overlayMine.classList.remove("hidden");
+});
+
+closeMine.addEventListener("click", closeMineWindow);
+
+overlayMine.addEventListener("click", closeMineWindow);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !mineWindow.classList.contains("hidden")) {
+    closeMineWindow();
+  }
+});
+
+
+const anagramWindow = document.querySelector(".anagram-window");
+const showAnagram = document.querySelector(".show-anagram");
+const closeAnagram = document.querySelector(".close-anagram");
+const overlayAnagram = document.querySelector(".overlay-anagram");
+
+const closeAnagramWindow = function() {
+  anagramWindow.classList.add("hidden");
+  overlayAnagram.classList.add("hidden");
+};
+
+showAnagram.addEventListener("click", function () {
+  anagramWindow.classList.remove("hidden");
+  overlayAnagram.classList.remove("hidden");
+});
+
+closeAnagram.addEventListener("click", closeAnagramWindow);
+
+overlayAnagram.addEventListener("click", closeAnagramWindow);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !anagramWindow.classList.contains("hidden")) {
+    closeAnagramWindow();
+  }
+});
+
+
+const pigWindow = document.querySelector(".pig-window");
+const showPig = document.querySelector(".show-pig");
+const closePig = document.querySelector(".close-pig");
+const overlayPig = document.querySelector(".overlay-pig");
+
+const closePigWindow = function() {
+  pigWindow.classList.add("hidden");
+  overlayPig.classList.add("hidden");
+};
+
+showPig.addEventListener("click", function () {
+  pigWindow.classList.remove("hidden");
+  overlayPig.classList.remove("hidden");
+});
+
+closePig.addEventListener("click", closePigWindow);
+
+overlayPig.addEventListener("click", closePigWindow);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !pigWindow.classList.contains("hidden")) {
+    closePigWindow();
+  }
+});
