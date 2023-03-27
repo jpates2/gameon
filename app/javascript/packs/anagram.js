cab2ef27f80f22380ab1f3fac4a0d5caa06684ed
Jess Pates <jesspates@hotmail.co.uk>
2023-03-27 21:53:21 +0200

    anagram random word added, input box and go button

--- app/javascript/packs/anagram.js
+++ app/javascript/packs/anagram.js
@@ -1,11 +1,30 @@
 const anagramPlayButton = document.querySelector(".anagram-play-button");
 const anagramPlayButtonContainer = document.querySelector(".anagram-play-button-container");
+const anagramGame = document.querySelector(".anagram-game");
+const anagramWord = document.querySelector(".anagram-word");
+const anagramCheck = document.querySelector(".anagram-check");
 
 const startAnagram = function() {
   anagramPlayButtonContainer.classList.add("fade-out");
-  console.log("hide");
+  anagramGame.classList.add("fade-in");
+  anagramGame.classList.remove("hidden-delay");
+  anagramWord.innerHTML = "_   ".repeat(randomAnagram(anagramWordbank).length);
 }
 
 if (anagramPlayButton) {
   anagramPlayButton.addEventListener("click", startAnagram);
 }
+
+// const anagramWordbank = [["melon", "lemon"], ["listen", "silent"], ["players", "parsley"], ["deductions", "discounted"], ["resistance", "ancestries"]];
+const anagramWordbank = {
+  melon: "lemon",
+  listen: "silent",
+  players: "parsley",
+  deductions: "discounted",
+  resistance: "ancestries"
+};
+
+const randomAnagram = function (obj) {
+  const keys = Object.keys(obj);
+  return obj[keys[ keys.length * Math.random() << 0]];
+};
