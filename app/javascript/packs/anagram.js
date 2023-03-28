a444ba6780e93e76ade79c24b10a2483339cc4b1
Jess Pates <jesspates@hotmail.co.uk>
2023-03-28 14:22:01 +0200

    anagram result message

--- app/javascript/packs/anagram.js
+++ app/javascript/packs/anagram.js
@@ -2,7 +2,9 @@ const anagramPlayButton = document.querySelector(".anagram-play-button");
 const anagramPlayButtonContainer = document.querySelector(".anagram-play-button-container");
 const anagramGame = document.querySelector(".anagram-game");
 const anagramWord = document.querySelector(".anagram-word");
-const anagramCheck = document.querySelector(".anagram-check");
+const anagramGoButton = document.querySelector(".anagram-go-button");
+const anagramGuess = document.querySelector("#user-anagram-guess");
+const anagramResult = document.querySelector(".anagram-result");
 
 const startAnagram = function() {
   anagramPlayButtonContainer.classList.add("fade-out");
@@ -25,7 +27,47 @@ const anagramWordbank = {
   resistance: "ancestries"
 };
 
+let anagramPair;
+
 const randomAnagram = function (obj) {
   const keys = Object.keys(obj);
-  return obj[keys[ keys.length * Math.random() << 0]];
+  const randomAnagram = keys[Math.floor(Math.random() * keys.length)];
+  anagramPair = obj[`${randomAnagram}`];
+  return randomAnagram;
 };
+
+// const randomAnagram = function (obj) {
+//   const keys = Object.keys(obj);
+//   return obj[keys[ keys.length * Math.random() << 0]];
+// };
+
+// const randomAnagram = function (obj) {
+//   const entries = Object.entries(obj);
+
+//   for (const [key, value] of entries) {
+//     console.log(key);
+//     console.log(value);
+//     return key[key.length * Math.random() << 0];
+//   }
+// };
+
+// const checkAnagram = function() {
+//   if (anagramGuess.value.toLowerCase() === anagramWord.innerText.toLowerCase().replace(/\W/g, ''))
+//     console.log(true);
+// }
+
+const checkAnagram = function() {
+  if (anagramGuess.value.toLowerCase() === anagramPair) {
+    console.log(true);
+    anagramGoButton.innerText = "PLAY AGAIN";
+    anagramResult.innerText = "CORRECT, YOU WIN!"
+  } else {
+    anagramResult.innerText = "INCORRECT ANSWER, TRY AGAIN"
+    console.log(false);
+  }
+  anagramGuess.value = "";
+}
+
+if (anagramGoButton) {
+  anagramGoButton.addEventListener("click", checkAnagram);
+}
