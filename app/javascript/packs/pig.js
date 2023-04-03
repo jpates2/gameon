8b18f0557ce8794f7d1846a217b7a4570f752b33
Jess Pates <jesspates@hotmail.co.uk>
2023-04-03 23:32:02 +0200

    pig start game logic

--- app/javascript/packs/pig.js
+++ app/javascript/packs/pig.js
@@ -1,7 +1,17 @@
 const pigPlayButton = document.querySelector(".pig-play-button");
 const pigPlayButtonContainer = document.querySelector(".pig-play-button-container");
 const pigGame = document.querySelector(".pig-game")
+const pigBtnRoll = document.querySelector(".pig-btn-roll")
+const pigBtnHold = document.querySelector(".pig-btn-hold")
+const pigPlayer1 = document.querySelector(".pig-player-1")
+const pigPlayer2 = document.querySelector(".pig-player-2")
+const pigScore1 = document.querySelector(".pig-score-1")
+const pigScore2 = document.querySelector(".pig-score-2")
+const pigCurrent1 = document.querySelector(".pig-current-1")
+const pigCurrent2 = document.querySelector(".pig-current-2")
+const pigDice = document.querySelector(".fa-dice")
 
+let scores, currentScore, activePlayer, playing;
 
 const startPig = function() {
   pigPlayButtonContainer.classList.add("fade-out");
@@ -12,3 +22,29 @@ const startPig = function() {
 if (pigPlayButton) {
   pigPlayButton.addEventListener("click", startPig);
 }
+
+const initialisePig = function () {
+  pigDice.classList.add("fade-out");
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  pigScore1.textContent = 0;
+  pigScore2.textContent = 0;
+  pigCurrent1.textContent = 0;
+  pigCurrent2.textContent = 0;
+}
+
+if (pigBtnRoll) {
+  pigBtnRoll.addEventListener("click", initialisePig)
+}
+
+const switchPlayer = function () {
+  document.querySelector(`pig-current-${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  pigPlayer1.classList.toggle("pig-active-player");
+  pigPlayer2.classList.toggle("pig-active-player");
+};
