c0a61afd9aec4231b5173beb32dd6951533236a5
Jess Pates <jesspates@hotmail.co.uk>
2023-04-04 21:37:34 +0200

    pig game winner and game end logic logic

--- app/javascript/packs/pig.js
+++ app/javascript/packs/pig.js
@@ -3,14 +3,17 @@ const pigPlayButtonContainer = document.querySelector(".pig-play-button-containe
 const pigGame = document.querySelector(".pig-game")
 const pigBtnRoll = document.querySelector(".pig-btn-roll")
 const pigBtnHold = document.querySelector(".pig-btn-hold")
+const pigPlayer0 = document.querySelector(".pig-player-0")
 const pigPlayer1 = document.querySelector(".pig-player-1")
-const pigPlayer2 = document.querySelector(".pig-player-2")
+const pigScore0 = document.querySelector(".pig-score-0")
 const pigScore1 = document.querySelector(".pig-score-1")
-const pigScore2 = document.querySelector(".pig-score-2")
+const pigCurrent0 = document.querySelector(".pig-current-0")
 const pigCurrent1 = document.querySelector(".pig-current-1")
-const pigCurrent2 = document.querySelector(".pig-current-2")
+const pigRound0 = document.querySelector(".pig-round-0")
+const pigRound1 = document.querySelector(".pig-round-1")
 const pigShowDice = document.querySelector(".fa-dice")
 const pigDice = document.querySelector(".pig-dice")
+const pigPlayAgainButton = document.querySelector(".pig-play-again-button")
 
 let scores, currentScore, activePlayer, playing;
 
@@ -18,8 +21,6 @@ const startPig = function() {
   pigPlayButtonContainer.classList.add("fade-out");
   pigGame.classList.add("fade-in");
   pigGame.classList.remove("hidden-delay");
-  // pigShowDice.classList.remove("hidden");
-  // pigShowDice.classList.add("fade-out-slow");
 }
 
 if (pigPlayButton) {
@@ -32,20 +33,20 @@ const initialisePig = function () {
   activePlayer = 0;
   playing = true;
 
+  pigScore0.textContent = 0;
   pigScore1.textContent = 0;
-  pigScore2.textContent = 0;
+  pigCurrent0.textContent = 0;
   pigCurrent1.textContent = 0;
-  pigCurrent2.textContent = 0;
 }
 
 initialisePig();
 
 const switchPlayer = function () {
-  document.querySelector(`.pig-current-${activePlayer + 1}`).textContent = 0;
+  document.querySelector(`.pig-current-${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
+  pigPlayer0.classList.toggle("pig-active-player");
   pigPlayer1.classList.toggle("pig-active-player");
-  pigPlayer2.classList.toggle("pig-active-player");
 };
 
 const pigRoll = function() {
@@ -67,7 +68,7 @@ const pigRoll = function() {
 
     if (dice !== 1) {
       currentScore += dice;
-      document.querySelector(`.pig-current-${activePlayer + 1}`).textContent = currentScore;
+      document.querySelector(`.pig-current-${activePlayer}`).textContent = currentScore;
     } else {
       switchPlayer();
     }
@@ -78,4 +79,35 @@ if (pigBtnRoll) {
   pigBtnRoll.addEventListener("click", pigRoll)
 }
 
-console.log(`pig-current-${activePlayer + 1}`);
+
+const pigHold = function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.querySelector(`.pig-score-${activePlayer}`).textContent = scores[activePlayer];
+  }
+
+  if (scores[activePlayer] >= 20) {
+    playing = false;
+    document.querySelector(`.pig-player-${activePlayer}`).classList.add("pig-winner");
+    document.querySelector(`.pig-player-${activePlayer}`).classList.remove("pig-active-player");
+    document.querySelector(`.pig-round-${activePlayer}`).style.color = "black";
+    document.querySelector(`.pig-current-${activePlayer}`).textContent = "WINNER";
+    pigDice.classList.remove("fa-dice-one");
+    pigDice.classList.remove("fa-dice-two");
+    pigDice.classList.remove("fa-dice-three");
+    pigDice.classList.remove("fa-dice-four");
+    pigDice.classList.remove("fa-dice-five");
+    pigDice.classList.remove("fa-dice-six");
+    pigDice.classList.remove("fa-solid");
+    pigDice.classList.add("pig-dice");
+    pigShowDice.classList.remove("hidden");
+    pigPlayAgainButton.classList.remove("hidden");
+    // pigBtnRoll.classList.add
+  } else {
+    switchPlayer();
+  }
+}
+
+if (pigBtnHold) {
+  pigBtnHold.addEventListener("click", pigHold)
+}
