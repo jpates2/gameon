7968db261878954236a0ce8a781353a18768bf89
Jess Pates <jesspates@hotmail.co.uk>
2023-04-23 00:03:44 +0200

    minesweeper numbers

--- app/javascript/packs/mine.js
+++ app/javascript/packs/mine.js
@@ -4,6 +4,7 @@ const mineGame = document.querySelector(".mine-game");
 const mineTable = document.querySelector(".mine-table");
 
 let playing, bomb;
+let bombs = [];
 
 const startMine = function() {
   minePlayButtonContainer.classList.add("fade-out");
@@ -12,7 +13,6 @@ const startMine = function() {
   playing = true;
   bomb = "💣";
   buildMineTable();
-  placeBomb();
 }
 
 if (minePlayButton) {
@@ -28,27 +28,25 @@ const buildMineTable = function() {
     let row = document.createElement("tr");
     for (j = 0; j < 10; j++) {
       let column = document.createElement("td");
-      column.setAttribute("class", "mine-board-box");
+      column.setAttribute("class", "mine-board-box mine-cell-hidden");
       column.id = cellID(i, j);
       row.appendChild(column);
     }
     mineTable.appendChild(row);
   }
-}
 
-const placeBombs = function() {
-  const rows = [];
+  placeBomb();
 
-  for (i = 0; i < 10; i++) {
-      placeSingleBomb(rows);
-  }
-  return rows;
+  const mineBoard = document.querySelectorAll(".mine-board-box");
+
+  for (let i = 0; i < mineBoard.length; i++)
+    mineBoard[i].addEventListener("click", revealMineCell);
+
 }
 
 let currentBomb;
 
 const placeBomb = function() {
-  let bombs = [];
 
   for (i = 0; i < 10; i++) {
     bombs.push([Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)]);
@@ -56,23 +54,26 @@ const placeBomb = function() {
 
   bombs.forEach (function(bomb) {
     currentBomb = document.getElementById(`cell-${bomb[0]}-${bomb[1]}`);
-    currentBomb.textContent = "💣";
-    currentBomb.classList.add("mine-bomb-hidden");
+    currentBomb.classList.add("mine-cell-bomb");
   })
-
-  const mineBoard = document.querySelectorAll(".mine-board-box");
-
-  const revealMineBomb = function (e) {
-    console.log("bomb test");
-    // currentBomb.classList.remove("mine-bomb-hidden");
-    // const mineCell = e.target;
-    // console.log(bombs[parseInt(e.target.parentElement.rowIndex)][parseInt(e.target.cellIndex)]);
-    // bombs[parseInt(e.target.parentElement.rowIndex)][parseInt(e.target.cellIndex)].classList.remove("mine-bomb-hidden");
-  }
-
-  for (let i = 0; i < mineBoard.length; i++)
-    mineBoard[i].addEventListener("click", revealMineBomb);
 
   console.log(bombs);
   console.log(`cell-${bombs[0][0]}-${bombs[0][1]}`);
+}
+
+const revealMineCell = function (e) {
+  const mineCell = e.target;
+
+  if (mineCell.classList.contains("mine-cell-bomb")) {
+    mineCell.textContent = "💣";
+  } else {
+    mineCell.textContent = "1";
+    mineCell.classList.add("mine-cell-number")
+  }
+
+  // document.getElementById(`cell-${mineCell.parentElement.rowIndex}-${mineCell.cellIndex}`).classList.remove("mine-cell-hidden")
+  // mineCell.classList.remove("mine-cell-hidden");
+
+  console.log(mineCell);
+  console.log(mineCell.parentElement.rowIndex, mineCell.cellIndex);
 }
