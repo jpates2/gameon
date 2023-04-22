0c83dccbe9eb2128f27e06d4769e2aa4c54539d0
Jess Pates <jesspates@hotmail.co.uk>
2023-04-22 17:10:31 +0200

    minesweeper random allocation bombs

--- app/javascript/packs/mine.js
+++ app/javascript/packs/mine.js
@@ -3,16 +3,24 @@ const minePlayButtonContainer = document.querySelector(".mine-play-button-contai
 const mineGame = document.querySelector(".mine-game");
 const mineTable = document.querySelector(".mine-table");
 
+let playing, bomb;
+
 const startMine = function() {
-  console.log("mine test");
   minePlayButtonContainer.classList.add("fade-out");
   mineGame.classList.add("fade-in");
   mineGame.classList.remove("hidden-delay");
+  playing = true;
+  bomb = "💣";
   buildMineTable();
+  placeBomb();
 }
 
 if (minePlayButton) {
   minePlayButton.addEventListener("click", startMine);
+}
+
+const cellID = function(i, j) {
+  return 'cell-' + i + '-' + j;
 }
 
 const buildMineTable = function() {
@@ -20,16 +28,38 @@ const buildMineTable = function() {
     let row = document.createElement("tr");
     for (j = 0; j < 10; j++) {
       let column = document.createElement("td");
-      column.setAttribute("class", "mine-board-box")
+      column.setAttribute("class", "mine-board-box");
+      column.id = cellID(i, j);
       row.appendChild(column);
     }
     mineTable.appendChild(row);
   }
+}
 
-  // for (i = 0; i <= 1; i++) {
-  //   mineTable.insertAdjacentHTML("afterbegin", `<tr class = "mine-board-row"></tr>`);
-  //   for (j = 0; j <= 1; j++) {
-  //     mineTable.insertAdjacentHTML("afterbegin", `<td class = "mine-board-box"></td>`);
-  //   }
-  // }
+const placeBombs = function() {
+  const rows = [];
+
+  for (i = 0; i < 10; i++) {
+      placeSingleBomb(rows);
+  }
+  return rows;
+}
+
+let currentBomb;
+
+function placeBomb() {
+  let bombs = [];
+
+  for (i = 0; i < 10; i++) {
+    bombs.push([Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)]);
+  }
+
+  bombs.forEach (function(bomb) {
+    // document.getElementById(`cell-${bomb[0]}-${bomb[1]}`).textContent = "b";
+    document.getElementById(`cell-${bomb[0]}-${bomb[1]}`).classList.add("mineBomb");
+    }
+  )
+
+  console.log(bombs);
+  console.log(`cell-${bombs[0][0]}-${bombs[0][1]}`);
 }
