a082c82ac6f6bf64b0241cf191960531970900fd
Jess Pates <jesspates@hotmail.co.uk>
2023-05-07 17:45:35 +0200

    flash intro msg

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -2,11 +2,21 @@ const flashPlayButton = document.querySelector(".flash-play-button");
 const flashPlayAgainButton = document.querySelector(".flash-play-again-button");
 const flashPlayButtonContainer = document.querySelector(".flash-play-button-container");
 const flashGame = document.querySelector(".flash-game");
+const flashStartMsg = document.querySelector(".flash-start-msg");
 
 const startFlash = function() {
   flashPlayButtonContainer.classList.add("fade-out");
   flashGame.classList.add("fade-in");
   flashGame.classList.remove("hidden-delay");
+  flashStartMessage();
+}
+
+function flashStartMessage() {
+  flashStartMsg.textContent = "GET READY";
+
+  setTimeout(function(){
+    flashStartMsg.textContent = "";
+  }, 4000);
 }
 
 if (flashPlayButton) {
