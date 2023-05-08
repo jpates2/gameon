7a4545b314ee6d05c305cf433884ff9807b43fb4
Jess Pates <jesspates@hotmail.co.uk>
2023-05-08 22:03:51 +0200

    flash grid added

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -3,22 +3,41 @@ const flashPlayAgainButton = document.querySelector(".flash-play-again-button");
 const flashPlayButtonContainer = document.querySelector(".flash-play-button-container");
 const flashGame = document.querySelector(".flash-game");
 const flashStartMsg = document.querySelector(".flash-start-msg");
+const flashTable = document.querySelector(".flash-table");
 
 const startFlash = function() {
   flashPlayButtonContainer.classList.add("fade-out");
   flashGame.classList.add("fade-in");
   flashGame.classList.remove("hidden-delay");
   flashStartMessage();
+  buildFlashGrid();
 }
 
 function flashStartMessage() {
-  flashStartMsg.textContent = "GET READY";
+  flashStartMsg.textContent = "GET READY...";
 
   setTimeout(function(){
     flashStartMsg.textContent = "";
-  }, 4000);
+  }, 3000);
 }
 
 if (flashPlayButton) {
   flashPlayButton.addEventListener("click", startFlash);
+}
+
+const flashCellID = function(i, j) {
+  return 'flashcell-' + i + '-' + j;
+}
+
+const buildFlashGrid = function() {
+  for (i = 0; i < 5; i++) {
+    let row = document.createElement("tr");
+    for (j = 0; j < 5; j++) {
+      let column = document.createElement("td");
+      column.setAttribute("class", "flash-grid");
+      column.id = flashCellID(i, j);
+      row.appendChild(column);
+    }
+    flashTable.appendChild(row);
+  }
 }
